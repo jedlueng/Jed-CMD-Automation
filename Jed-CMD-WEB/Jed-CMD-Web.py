132837a4a9d2eb36d6da87f0f41b0341bfe40130
Jedsada Luengaramsuk <68596277+jedsada-io@users.noreply.github.com>
2021-07-13 23:40:47 +0700

    Create a new file

---
+++ Jed-CMD-WEB/Jed-CMD-Web.py
@@ -0,0 +1,9 @@
+#Started 13 July 2021 
+#Jedsada Luengaramsuk (Jedsada.io)
+#Goal: Type {JCW.py} {Name} to get to the right browser and right webpage 
+
+#Use sys.arge for cmd
+
+Browser = ''
+Page = ''
+
