4ae7f7a82b9687180a9a89df7b83be9357117b59
Jedsada Luengaramsuk <68596277+jedsada-io@users.noreply.github.com>
2021-08-09 13:46:25 +0700

    Add input, comments and sites

--- Jed-CMD-Web.py
+++ Jed-CMD-Web.py
@@ -1,34 +1,36 @@
-#Started 13 July 2021 
-#Jedsada Luengaramsuk (Jedsada.io)
+#Started 13 July 2021
+#Finished 9 August 2021  
+#Jedsada Luengaramsuk 
 #Goal: Type {JCW.py} {Name} to get to the right browser and right webpage 
-
 #Use sys.arge for cmd
 #Use Sherbang Line to run python automatically 
 #Use webbrowser module 
-
-
 #! /usr/bin/env python3
-
-import webbrowser, pyperclip, sys
-
+import webbrowser, sys
+#Initialize each webbrowser with each path 
 chrome = webbrowser.get("open -a /Applications/Chrome.app %s")
 brave =  webbrowser.get("open -a /Applications/Brave.app %s")
-Firefox = webbrowser.get("open -a /Applications/Developer.app %s")
+firefox = webbrowser.get("open -a /Applications/Developer.app %s")
 Safari = webbrowser.get("open -a /Applications/Safari.app %s")
 
+#Input box 
+site = str(input('What do you want to open?'))
 
-if len(sys.argv) > 1: 
-    #Get address from command line 
-    site = ' '.join(sys.argv[1:])
-else:
-    #Get address from clipboard 
-    site = pyperclip.paste()
-
-def open (a):
-    if site == 'coinmarketcap': 
-        brave.open_new('https://coinmarketcap.com/')
-    else: 
-        print(None)
-
+if site == 'coin': 
+    brave.open_new('https://coinmarketcap.com/')
+elif site == 'amazon':
+    chrome.open_new('https://amazon.co.uk/')
+elif site == 'pareto': 
+    brave.open_new('https://twitter.com/')
+elif site == 'jjlueng': 
+    chrome.open_new('https://twitter.com/')
+elif site == 'codea': 
+    firefox.open_new('https://www.codecademy.com/') #codeacademy 
+elif site == 'comp': 
+    firefox.open_new('https://learn.comptia.org/login#study-plan/structured')#comptia-training 
+elif site == 'course': 
+    chrome.open_new('https://coursera.org')#coursera
+else: 
+    print('Not in directory')
 
 
